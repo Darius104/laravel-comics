69abfed853e7cf1c2b4fd446b798f88a440aba4d
Darius104 <comandarius@gmail.com>
2022-02-09 17:08:48 +0100

    Header done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('page_title')
+    Comic Page
+@endsection
+
 @section('main_content')
     <h1>prova main</h1>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,23 @@
 <header>
-    ciao sono header
+    <div class="container">
+        <div class="_head">
+            <img class="logo" src="{{ asset('images/dc-logo.png') }}" alt="">
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
+    </div>
+    
+    <div>
+        <img class="banner" src="{{ asset('images/jumbotron.jpg') }}" alt="">
+    </div>
 </header>
